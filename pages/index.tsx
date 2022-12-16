d0357057262971350731782a59c3915087e4acb8
Israel S. Santos <israelss@primeit.com.br>
2022-12-15 23:15:52 -0300

    before fix loo´ping

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState, FormEvent } from 'react';
+import React, { useRef, useEffect, useState, FormEvent, use } from 'react';
 import mapboxgl from "mapbox-gl";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import styles from '../styles/Home.module.css'
@@ -11,32 +11,25 @@ interface coordI {
   lngCoord: number,
   latCoord: number,
 }
-interface reloadedI {
-  reloaded: boolean,
-  setReloaded: boolean
-}
-interface dbDataI {
-  indicadora: string,
-  latCoord: number,
-  lngCoord: number,
-  nome: string
-}
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiaXNyYWVsc29hcmVzIiwiYSI6ImNrdmUyeDhsM2JkYm4yem1udDJ4azA3cnkifQ.Zlsg1JsFYg85GMH6qSAPvQ";
 
 function App(): JSX.Element {
 
-  const [nome, setNome] = useState('');
-  const [indicadora, setIndicadora] = useState('');
-  const [latCoord, setLatCoord] = useState<any>()
-  const [lngCoord, setLngCoord] = useState<any>()
+  const [nome, setNome] = useState<any>('');
+  const [indicadora, setIndicadora] = useState<any>('');
+  const [updating, setUpdating] = useState<any>(false);
+  const [indicate, setIndicate] = useState<any>('');
+  const [latCoord, setLatCoord] = useState<any>('')
+  const [lngCoord, setLngCoord] = useState<any>('')
   const mapContainer = useRef<any>(null);
   const map = useRef<any>(null);
   const [reloaded, setReloaded] = useState<any>();
   const [lng, setLng] = useState<number>(0);
   const [lat, setLat] = useState<number>(0);
   const [zoom, setZoom] = useState<number>(2);
+  const [isUpdating, setIsUpdating] = useState<boolean>(false)
   const markerHeight = 50;
   const markerRadius = 50;
   const linearOffset = 25;
@@ -56,16 +49,31 @@ function App(): JSX.Element {
     const ref = database.ref('empresa');
     ref.on('value', (result: any) => {
       if (result) {
-        let datas: void = Object.entries(result.val()).forEach((val: any) => {
+        const datas: void = Object.entries(result.val()).forEach((val: any) => {
           const popup = new mapboxgl.Popup({ offset: popupOffsets, className: styles.details })
             .setLngLat([val[1]?.lngCoord, val[1]?.latCoord])
-            .setHTML(`<h3>Nome: ${val[1]?.nome} </br>Pontos: 0</br>lat: ${val[1]?.latCoord} lng: ${val[1]?.lngCoord}</h3>`)
+            .setHTML(`<h3>Nome: ${val[1]?.nome} </br>Pontos: ${val[1]?.point}</br>lat: ${val[1]?.latCoord} lng: ${val[1]?.lngCoord}</h3>`)
             .setMaxWidth("300px")
             .addTo(map.current);
-        });
+        })
       }
     })
   }
+  const updatePoint = () => {
+    const ref = database.ref('empresa');
+    ref.on('value', (val: any) => {
+      const values = Object.entries(val.val()).forEach((val: any) => {
+        if (val[1]?.nome == indicate) {
+          ref.child(`${val[0]}`).update({ point: val[1]?.point + 10 })
+          setUpdating(false);
+        }
+      })
+    })
+  }
+
+  useEffect(() => {
+    updatePoint();
+  }, [updating])
 
 
   useEffect(() => {
@@ -79,23 +87,26 @@ function App(): JSX.Element {
 
   useEffect(() => {
     getAllData();
+    setIndicadora('');
+    setNome('');
+    setLatCoord('');
+    setLngCoord('');
   }, [reloaded])
 
   const sendData = (e: FormEvent) => {
     e.preventDefault()
+    const point = 0
     const data: any = {
       nome,
       indicadora,
       latCoord,
-      lngCoord
+      lngCoord,
+      point,
     }
     const ref = database.ref('empresa')
     ref.push(data)
-    setNome('');
-    setIndicadora('');
-    setLatCoord(0);
-    setLngCoord(0);
     setReloaded(!reloaded);
+    setUpdating(true);
   }
 
 
@@ -103,11 +114,14 @@ function App(): JSX.Element {
     <div className={styles.form}>
       <form onSubmit={sendData}>
         <h3>Cadastre uma empresa</h3>
-        <input value={nome} type="text" placeholder="Nome da Empresa:" onChange={(e) => setNome(e.target.value)} />
-        <input value={indicadora} type="text" placeholder="Empresa indicadora:" onChange={(e) => setIndicadora(e.target.value)} />
+        <input required value={nome} type="text" placeholder="Nome da Empresa:" onChange={(e) => setNome(e.target.value)} />
+        <input required value={indicadora} type="text" placeholder="Empresa indicadora:" onChange={(e) => {
+          setIndicadora(e.target.value);
+          setIndicate(e.target.value);
+        }} />
         <div className={styles.box_form}>
-          <input value={latCoord} type="number" placeholder="Latitude:" onChange={(e) => setLatCoord(e.target.value)} />
-          <input value={lngCoord} type="number" placeholder="Longitude:" onChange={(e) => setLngCoord(e.target.value)} />
+          <input required value={latCoord} type="number" max={90} min={-90} placeholder="Latitude:" onChange={(e) => setLatCoord(e.target.value)} />
+          <input required value={lngCoord} type="number" max={90} min={-90} placeholder="Longitude:" onChange={(e) => setLngCoord(e.target.value)} />
         </div>
         <button type='submit'>Cadastrar</button>
       </form>
